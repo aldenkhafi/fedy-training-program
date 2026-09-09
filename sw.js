f3377406b0e4a758c13ba0dd4b1accf16a194209
aldenkhafi <aldenkhafi0203@gmail.com>
2026-09-09 20:51:10 +0700

    Rename cache and update app shell icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='training-program-pwa-v1';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='fitness-journey-training-admin-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
